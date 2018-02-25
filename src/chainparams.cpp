9794d983c564d0cf37ce938386625bfb5146916f
gkc.im <gkc.im@icloud.com>
2018-02-25 19:02:02 +0800

    modify net code

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -111,10 +111,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0x67;  // g
+        pchMessageStart[1] = 0x6b;  // k
+        pchMessageStart[2] = 0x63;  // c
+        pchMessageStart[3] = 0x6d;  // m
         nDefaultPort = 5333;
         nPruneAfterHeight = 100000;
 
